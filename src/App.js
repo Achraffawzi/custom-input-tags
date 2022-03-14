1c56845b849a8375b3d0e8ae13c1a9d12facd3d8
achraffawzi <achraffawzi224a@gmail.com>
2022-03-14 10:48:34 +0100

    final push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import "./app.css";
+import { FaTags } from "react-icons/fa";
 
 function App() {
+  const tagsWrapper = useRef();
+  const inputRef = useRef();
+
+  const [tags, setTags] = useState([]);
+  const [tag, setTag] = useState("");
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter" && tags.length < 6 && tag.length > 0) {
+      setTags((tags) => [...tags, tag]);
+      setTag("");
+    }
+  };
+
+  const handleRemoveTag = (e) => {
+    if (e.key === "Backspace" && tag === "") {
+      setTags((oldTags) => oldTags.slice(0, oldTags.length - 1));
+    }
+  };
+
+  const handleRemove = (singleTag) => {
+    setTags((oldTags) => oldTags.filter((tag) => tag !== singleTag));
+  };
+
+  const handleRemoveAll = () => {
+    setTags([]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      <h3 className="app-title">
+        <FaTags />
+        <span>Tags</span>
+      </h3>
+      <div
+        className="tags-wrapper"
+        ref={tagsWrapper}
+        onClick={() => inputRef.current.focus()}
+      >
+        <div className="tags-selected">
+          {tags.map((singleTag, index) => (
+            <span className="single-tag" key={index}>
+              {singleTag}{" "}
+              <span className="close" onClick={() => handleRemove(singleTag)}>
+                &times;
+              </span>
+            </span>
+          ))}
+        </div>
+        <input
+          type="text"
+          className="tags-input"
+          ref={inputRef}
+          value={tag}
+          onChange={(e) => setTag(e.target.value)}
+          onKeyPress={(e) => handleKeyPress(e)}
+          onKeyDown={(e) => handleRemoveTag(e)}
+          placeholder="Add a tag"
+        />
+      </div>
+      <div className="bottom">
+        <p>{6 - tags.length} tags remaining</p>
+        <button
+          onClick={() => handleRemoveAll()}
+          type="button"
+          className="delete-button"
         >
-          Learn React
-        </a>
-      </header>
+          remove all tags
+        </button>
+      </div>
     </div>
   );
 }
